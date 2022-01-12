d4b2e8b15815749359394daf8d2038aee8430e22
= <=>
2022-01-12 14:05:35 -0800

    Testing page posts

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ function createPostContent() {
 
 }
 
+async function getPage (pageId, accessToken) {
+    let response = await fetch(`https://graph.facebook.com/${pageId}/feed?published=false&message=An unpublished post&access_token=${accessToken}`).then((response) => response.json());
+
+    return response;
+}
+
 function insertPostPreviews(posts) {
     let wrapDiv = document.querySelector('#post-preview-wrapper');
 
@@ -25,10 +31,3 @@ function insertPostPreviews(posts) {
 
     return;
 }
-
-postPreviews = [
-    {
-        message: "Happy birthday Ian",
-        imageUrl: "https://s3.amazonaws.com/SmartsheetB1/cf95bb540c484822bf9c716f37eeace8?response-content-disposition=inline%3Bfilename%3D%22Employee_Image.jpg%22%3Bfilename*%3DUTF-8%27%27Employee_Image.jpg&Signature=%2Ffo181dFxPy0ks4Jpvc3HhUm0CE%3D&Expires=1642022743&AWSAccessKeyId=11950YFEZZJFSSKKB3G2"
-    }
-]
